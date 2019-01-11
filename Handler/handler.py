049dfddb1b767599cf6c8dd7f9d6ac67f3ebd082
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2019-01-11 22:37:40 +0100

    Remove print statements

--- Handler/handler.py
+++ Handler/handler.py
@@ -7,13 +7,11 @@ import tornado.web
 import tornado.httpserver
 import tornado.ioloop
 
-print "here1"
 certDir = '../testCerts/'
 ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 ssl_ctx.load_cert_chain(certDir+'server.crt', certDir+'server.key')
 ssl_ctx.load_verify_locations(certDir+'CAcert.pem')
 ssl_ctx.load_verify_locations(certDir+'user.crt')
-print "here2?"
 # ssl_ctx.load_cert_chain("CA_test/localhost.cert.pem", "CA_test/localhost.key.pem")
 # ssl_ctx.load_verify_locations("CA_test/ca.cert.pem")
 # ssl_ctx.load_verify_locations("test/ca_client.cert.pem")
@@ -29,7 +27,6 @@ def cert_to_dict(cert):
 def DN_check(filename, client_DN):
     '''Comparing client DN in dictionary form to DN which are in text file'''
 
-    print "HERERE5!!"
     DN_file = open(filename, "r")
     cert_dictionary_list = []
     for line in DN_file:
@@ -45,11 +42,9 @@ class MainHandler(tornado.web.RequestHandler):
     """Request handler for the main page of the app localhost:1027"""
     def get(self):
         """Information about server which returns with  HTTP Get"""
-        print "HERERE4!!"
         self.render("MainPage.html")
     def post(self):
         """Post function"""
-        print "HERERE3!!"
         if self.get_argument('User') == "U" and self.get_argument('Password') == "R":
             self.write("Ok")
             self.redirect("https://localhost:1027/random")
@@ -59,20 +54,16 @@ class MainHandler(tornado.web.RequestHandler):
 class JsonHandler(tornado.web.RequestHandler):
     """Request handler for json messages"""
     def initialize(self):
-        print "HERERE!!"
         """Auth by cert"""
         client_cert = self.request.get_ssl_certificate()
         self.client_dn = cert_to_dict(client_cert)
-        print "here"
         if not DN_check("Test_DN", self.client_dn):
             self.finish()
     def get(self):
         """get function of jsonhandler"""
-        print "HERERE100!!"
         self.write(self.client_dn)
     def post(self):
         """post function of json handler"""
-        print "HERERE2!!"
         self.write(self.request.get_ssl_certificate())
         json_message = json.loads(self.request.body.decode('string-escape').strip('"'))
         #json.loads(json.loads(self.request.body)) #you can try with this too ^^
@@ -84,7 +75,7 @@ def make_app():
                                     (r"/json", JsonHandler)])
 
 if __name__ == "__main__":
-    print "STARTING"
+    print "STARTING TORNADO SERVER!"
     app = make_app()
     http_server = tornado.httpserver.HTTPServer(app, ssl_options=ssl_ctx)
     http_server.listen(1027)
