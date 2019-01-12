4fed2e6a9c7f45375284b0878ea6315474deaf14
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2019-01-13 00:06:00 +0100

    Change the format of the Test_DN file to list of dicts
    Also, change the function that loads the DNs

--- Handler/handler.py
+++ Handler/handler.py
@@ -15,18 +15,19 @@ def cert_to_dict(cert):
     cert_dict.update(dict(cert['subject'][i]))
   return cert_dict
 
+
 def valid_cert(client_cert, validDNs):
   '''Comparing client DN in dictionary form to DN which are in text file'''
   client_dn = cert_to_dict(client_cert)
+  print client_dn
   return client_dn in validDNs
 
-def getValidDNs(filename = 'Test_DN'):
-  fileDN = open(filename, "r")
-  cert_dictionary_list = []
-  for line in fileDN:
-    dictionary = json.loads(line)
-    cert_dictionary_list.append(dictionary)
-  return cert_dictionary_list
+
+def getValidDNs(filename='Test_DN'):
+  dnList = []
+  with open(filename, "r") as fileDN:
+    dnList = json.load(fileDN)
+  return dnList
 
 # pylint: disable = W0223, invalid-name, arguments-differ
 
@@ -38,7 +39,7 @@ class MainHandler(tornado.web.RequestHandler):
     """Auth by cert"""
     self.current_DNs = getValidDNs()
     client_cert = self.request.get_ssl_certificate()
-    if not valid_cert(client_cert, validDNs = self.current_DNs):
+    if not valid_cert(client_cert, validDNs=self.current_DNs):
       print "This certificate is not authorized!"
       self.finish()
 
@@ -51,12 +52,13 @@ class MainHandler(tornado.web.RequestHandler):
     self.write(self.request.get_ssl_certificate())
     message = json.loads(
         self.request.body.decode('string-escape').strip('"'))
-    self.sendMessage(message) # send message to MQ
+    self.sendMessage(message)  # send message to MQ
 
   def sendMessage(self, message):
-    #Just for a moment
+    # Just for a moment
     if message["source"] == "InstallDIRAC":
       self.write("\nSource is correct")
+
 
 class TestHandler(tornado.web.RequestHandler):
   """Request handler for the main page of the app localhost:1027"""
