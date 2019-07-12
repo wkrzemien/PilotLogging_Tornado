73a50118c1244fa07b682948c6c175ad078664ab
Danyil <potoka1998@gmail.com>
2019-07-12 14:38:30 +0200

    receiver => test_fin

--- tests/test_fin.py
+++ tests/test_fin.py
@@ -26,3 +26,17 @@ json_msg = json.dumps(msg, separators=(', ', ': '))
 r2 = requests.post(URL, cert=(client_cert, client_key),
                    verify=CAcert, json=json_msg)
 print r2.text
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672))
+channel = connection.channel()
+channel.queue_declare(queue='test', durable=True)
+channel.basic_consume(queue='test',
+                      auto_ack=True,
+                      on_message_callback=callback)
+
+print "Start of receiving"
+channel.start_consuming()
+
