6c2471b4c9782b7df97eeeb26d7d909f8fd5d937
Danyil <potoka1998@gmail.com>
2019-07-16 15:29:18 +0200

    options and config file

--- Handler/handler.py
+++ Handler/handler.py
@@ -3,16 +3,15 @@
    checks certificate and certficate_DN
    and if all is ok send message to message queue,
    configuration is described in handler_config.json"""
-import sys
 import ssl
 import json
 import requests
 import tornado.web
 import tornado.httpserver
 import tornado.ioloop
+from tornado.options import options, define
 from OpenSSL import crypto
 from stompSender import StompSender
-
 
 class ConfigError(Exception):
     """Error in configuration"""
@@ -22,17 +21,48 @@ class ConfigError(Exception):
         return repr(self.value)
 
 
-def verify(CA, cert):
+def verify(ca, cert):
     """This functions checks two certificates,
        which are OpenSSL.crypto.X509 variables
     """
     store = crypto.X509Store()
-    store.add_cert(CA)
+    store.add_cert(ca)
     store_context = crypto.X509StoreContext(store, cert)
-    if store_context.verify_certificate() is None:
-        return True
+    return True if store_context.verify_certificate() is None else False
+
+
+def readConfigFile(configuration_file):
+    """Read config file and return dict of params"""
+    try:
+        with open(configuration_file, 'r') as config_file:
+            params = json.load(config_file)
+            return params
+    except IOError:
+        raise IOError('I can`t find config file, please check it')
+
+def areParamsValid(config_json):
+    """Check if cert from params was signed by CA
+       and if hostname from params matches cert`s CN"""
+    app_host = config_json['host']
+    server_cert_file = open(config_json['server_cert'])
+    CA_cert_file = open(config_json['ca_cert'])
+    server_cert_data = server_cert_file.read()
+    CA_cert_data = CA_cert_file.read()
+    server_cert = crypto.load_certificate(crypto.FILETYPE_PEM, server_cert_data)
+    CA_cert = crypto.load_certificate(crypto.FILETYPE_PEM, CA_cert_data)
+    server_DN_crypto = server_cert.get_subject()
+    server_DN = dict(server_DN_crypto.get_components())
+    if server_DN['CN'] != app_host:
+        print 'Hostname:'
+        print app_host
+        print 'Certificate CN:'
+        print server_DN['CN']
+        raise ConfigError('Hostname doesn`t match certificates CN')
+    elif verify(CA_cert, server_cert) != True:
+        raise ConfigError('Certificate isn`t signed by provided CA')
     else:
-        return False
+        return True
+
 
 
 def extract_DN(cert):
@@ -53,14 +83,21 @@ def loadMQConfig(filename='handler_config.json'):
     """This function loads configuration for MQ from json file"""
     conf = {}
     try:
-        with open(filename) as myFile:
-            conf = json.load(myFile)
+        with open(filename) as config_file:
+            pre_conf = json.load(config_file)
+            conf['Host'] = pre_conf['mq_host']
+            conf['Port'] = pre_conf['mq_port']
+            conf['QueuePath'] = pre_conf['mq_queue']
+            conf['Username'] = pre_conf['mq_user']
+            conf['Password'] = pre_conf['mq_password']
     except IOError:
         pass
     return conf
 
 def getValidDNs_from_file(filename='Test_DN.json'):
     """This function create list of valid dn from json file"""
+    params = readConfigFile()
+    filename = params['dn_filename']
     dn_list = []
     try:
         with open(filename) as fileDN:
@@ -116,43 +153,21 @@ def generate_ssl_context(server_certificate, server_key, ca_cert):
     return mySSLContex
 if __name__ == "__main__":
 
-    #define("host", default="localhost", help="app host", type=str)
-    #define("port", default=8888, help="app port", type=int)
-    #define("certPath", default="../testCerts/", help="path to certificates", type=str)
-    #options.parse_command_line()
+    params = readConfigFile('handler_config.json')
 
-    ##todo it must be taken from some config file
-    #server_cert = options.certPath + 'test.crt'
-    #server_key = options.certPath + 'test.key'
-    #ca_cert = options.certPath +'CAcert.pem'
+    areParamsValid(params)
 
-    try:
-        with open('handler_config.json', 'r') as config_file:
-            config_json = json.load(config_file)
-            Tornado_host = config_json['Tornado_host']
-            server_cert_file = open(config_json['server_cert'])
-            CA_cert_file = open(config_json['CA_cert'])
-            server_cert_data = server_cert_file.read()
-            CA_cert_data = CA_cert_file.read()
-            server_cert = crypto.load_certificate(crypto.FILETYPE_PEM, server_cert_data)
-            CA_cert = crypto.load_certificate(crypto.FILETYPE_PEM, CA_cert_data)
-            server_DN_crypto = server_cert.get_subject()
-            server_DN = dict(server_DN_crypto.get_components())
-            if server_DN['CN'] != Tornado_host:
-                raise ConfigError('Hostname doesn`t match certificates CN')
-            else:
-                print 'hostname is ok'
-            if verify(CA_cert, server_cert):
-                print 'certificate is ok'
-            else:
-                raise ConfigError('Certificate isn`t signed by provided CA')
-    except IOError:
-        raise IOError('I can`t find config file, please check it')
+    define("host", default=params['host'], help="app host", type=str)
+    define("port", default=params['port'], help="app port", type=int)
+    define("server_cert", default=params['server_cert'], help="server certificate", type=str)
+    define("server_key", default=params['server_key'], help="server key", type=str)
+    define("CA_cert", default=params['ca_cert'], help="CA certificate", type=str)
+    options.parse_command_line()
 
-    print "STARTING TORNADO SERVER! Host:%s, Port:%i"%(config_json['Tornado_host'], config_json['Tornado_port'])
+    print "STARTING TORNADO SERVER! Host:%s, Port:%i"%(options.host, options.port)
 
     app = make_app()
-    ssl_ctx = generate_ssl_context(config_json['server_cert'], config_json['server_key'], config_json['CA_cert'])
+    ssl_ctx = generate_ssl_context(options.server_cert, options.server_key, options.CA_cert)
     http_server = tornado.httpserver.HTTPServer(app, ssl_options=ssl_ctx)
-    http_server.listen(config_json['Tornado_port'])
+    http_server.listen(options.port)
     tornado.ioloop.IOLoop.current().start()
